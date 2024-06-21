cf3cbce4ce8cfbbd89034083eb22c23a4d585019
Alberto Dopico <souto.varela02@gmail.com>
2024-06-21 13:06:40 +0200

    Ejercicio 01: ArrayList de Persona

--- src/adf/t01e03/Main.java
+++ src/adf/t01e03/Main.java
@@ -1,17 +1,78 @@
 package adf.t01e03;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.List;
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Persona> personas = new ArrayList<>();
+        personas.add(new Persona("12345678A", "Juan", 25));
+        personas.add(new Persona("87654321B", "Ana", 30));
+        personas.add(new Persona("11223344C", "Luis", 22));
+        personas.add(new Persona("44332211D", "Marta", 29));
+        personas.add(new Persona("55667788E", "Carlos", 28));
+        personas.add(new Persona("66778899F", "Elena", 33));
+
+        System.out.println("Age of the oldest person: " + getOldestAge(personas));
+        System.out.println("Average age: " + String.format("%.1f", getAverageAge(personas)));
+        System.out.println("Name of the oldest person: " + getOldestPersonName(personas));
+        System.out.println("Oldest person: " + getOldestPerson(personas));
+        System.out.println("All adults: " + getAllAdults(personas));
+        System.out.println("People older than the average: " + getAboveAverageAge(personas));
+    }
+
+    public static int getOldestAge(List<Persona> personas) {
+        int maxAge = 0;
+        for (Persona persona : personas) {
+            if (persona.getEdad() > maxAge) {
+                maxAge = persona.getEdad();
+            }
         }
+        return maxAge;
+    }
+
+    public static double getAverageAge(List<Persona> personas) {
+        int totalAge = 0;
+        for (Persona persona : personas) {
+            totalAge += persona.getEdad();
+        }
+        return (double) totalAge / personas.size();
+    }
+
+    public static String getOldestPersonName(List<Persona> personas) {
+        Persona oldestPerson = getOldestPerson(personas);
+        return oldestPerson.getNombre();
+    }
+
+    public static Persona getOldestPerson(List<Persona> personas) {
+        Persona oldestPerson = personas.getFirst();
+        for (Persona persona : personas) {
+            if (persona.getEdad() > oldestPerson.getEdad()) {
+                oldestPerson = persona;
+            }
+        }
+        return oldestPerson;
+    }
+
+    public static List<Persona> getAllAdults(List<Persona> personas) {
+        ArrayList<Persona> adults = new ArrayList<>();
+        for (Persona persona : personas) {
+            if (persona.getEdad() >= 18) {
+                adults.add(persona);
+            }
+        }
+        return adults;
+    }
+
+    public static List<Persona> getAboveAverageAge(List<Persona> personas) {
+        ArrayList<Persona> aboveAverage = new ArrayList<>();
+        double averageAge = getAverageAge(personas);
+        for (Persona persona : personas) {
+            if (persona.getEdad() >= averageAge) {
+                aboveAverage.add(persona);
+            }
+        }
+        return aboveAverage;
     }
 }
---
+++ src/adf/t01e03/Persona.java
@@ -0,0 +1,44 @@
+package adf.t01e03;
+
+public class Persona {
+    private final String dni;
+    private final String nombre;
+    private final int edad;
+
+    // Constructor
+    public Persona(String dni, String nombre, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return nombre + " [" + edad + " años]";
+    }
+
+    // equals
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Persona persona = (Persona) obj;
+        return dni.equals(persona.dni);
+    }
+
+    // hashCode
+    @Override
+    public int hashCode() {
+        return dni.hashCode();
+    }
+}
